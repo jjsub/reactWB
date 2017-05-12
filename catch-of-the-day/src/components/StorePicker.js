11ec43df31d1f720de9f53eba5c3ddc35dd230c0
Rodriguez <juan.j.rodriguez@accenture.com>
2017-05-12 14:44:59 -0500

    First commit

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <form> 
+              {/* This is how you do comment */}
+              <h2>Please Enter A Store</h2>
+              <input type="text" required placeholder="Store Name" />
+              <button type="submit">Visit Store</button>
+            </form>
+        )
+    }
+}
+
+export default StorePicker;
