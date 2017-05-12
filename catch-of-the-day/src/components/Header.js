7b2a4102f1d4b608a2fe8263ee0b6fa6423444e9
Rodriguez <juan.j.rodriguez@accenture.com>
2017-05-12 16:22:04 -0500

    Catch of the day

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 class Header extends React.Component{
     render() {
+                console.info(this);
+
         return (
-            <p>Header</p>
+            <header className="top">
+              <h1>
+                Catch 
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day
+              </h1>
+              <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+            </header>
         );
     }
 }
