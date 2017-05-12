fb679a2de0e819b2f6f2a9d3f7db761572968c96
Rodriguez <juan.j.rodriguez@accenture.com>
2017-05-12 16:00:10 -0500

    Create Header Inventory Order component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,7 @@ import React from 'react';
 class StorePicker extends React.Component {
     render() {
         return (
-            <form> 
+            <form className="store-selector"> 
               {/* This is how you do comment */}
               <h2>Please Enter A Store</h2>
               <input type="text" required placeholder="Store Name" />
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
+import './css/style.css';
+import App from './components/App';
 
 import StorePicker from './components/StorePicker' 
 
 
-render(<StorePicker />, document.getElementById('main'));
+render(<App />, document.getElementById('main'));
