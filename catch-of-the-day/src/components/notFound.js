d7ffdbbb99d6dd21401f196704e697ae594af87d
Rodriguez <juan.j.rodriguez@accenture.com>
2017-05-15 15:39:12 -0500

    Add Router pluming

---
+++ catch-of-the-day/src/components/notFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = (props) => {
+        return (
+            <h2>Not Found !1!1!</h2>
+        );
+}
+
+export default Header;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import notFound from './components/notFound'; 
 
-import StorePicker from './components/StorePicker' 
+const Root = () => {
+    return (
+
+          <BrowserRouter>
+            <div>
+              <Match exactly pattern="/" component={StorePicker} />
+              <Match pattern="/store/:storeId" component={App} />
+              <Miss component={notFound}   />
+            </div>  
+          </BrowserRouter>
+
+    );
+}
 
 
-render(<App />, document.getElementById('main'));
+render(<Root />, document.getElementById('main'));
