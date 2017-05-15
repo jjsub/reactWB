a9fa06342b2f57c201723495deedbea9004f1d57
Rodriguez <juan.j.rodriguez@accenture.com>
2017-05-15 15:07:57 -0500

    Stateless comp

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-class Header extends React.Component{
-    render() {
-                console.info(this);
-
+const Header = (props) => {
         return (
             <header className="top">
               <h1>
@@ -14,10 +11,9 @@ class Header extends React.Component{
                 </span>
                 Day
               </h1>
-              <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+              <h3 className="tagline"><span>{props.tagline}</span></h3>
             </header>
         );
-    }
 }
 
 export default Header;
