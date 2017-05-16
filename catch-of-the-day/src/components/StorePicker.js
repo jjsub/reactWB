e606717937ba7b7480be52fd4c37be664aa4434b
Rodriguez <juan.j.rodriguez@accenture.com>
2017-05-16 12:06:23 -0500

    Adding Ref

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    constructor(){
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('Change URL event', event);
+
+        console.info(this.storeInput.value);
+
+
+    }
     render() {
         return (
-            <form className="store-selector"> 
+            <form className="store-selector" onSubmit={this.goToStore}> 
               {/* This is how you do comment */}
               <h2>Please Enter A Store</h2>
-              <input type="text" required placeholder="Store Name" />
+              <input type="text" name="storeName" required ref={(input) => { this.storeInput = input}} placeholder="Store Name" defaultValue={getFunName()} />
               <button type="submit">Visit Store</button>
             </form>
         )
